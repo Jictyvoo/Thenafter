6e7d7c40da95966921bb68c8825781a6a75329d8
Jictyvoo <jictyvoo.ecomp@gmail.com>
2018-11-12 00:42:52 -0300

    Updated build.sh to build it to windows binaries

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 cd src
 luastatic main.lua controllers/ParserBNF.lua controllers/FirstGenerator.lua models/business/GenerateJava.lua models/business/GenerateLua.lua models/business/GeneratePython.lua /usr/lib/x86_64-linux-gnu/liblua5.3.a -I/usr/include/lua5.3
 mv main ../
+
+CC=x86_64-w64-mingw32-gcc luastatic main.lua controllers/ParserBNF.lua controllers/FirstGenerator.lua models/business/GenerateJava.lua models/business/GenerateLua.lua models/business/GeneratePython.lua ../lua5.3-lib/windows/liblua.a -I/usr/include/lua5.3 -lpthread
+strip main.exe
+mv main.exe ../
