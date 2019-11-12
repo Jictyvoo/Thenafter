059b2ce09dc220dda0a434e13c96258fc92c6e8e
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2019-11-12 17:27:41 -0300

    Update GeneratePython.lua
    Added get_production method, and get_first get_follow, because first and follow attributes became private

--- src/models/business/GeneratePython.lua
+++ src/models/business/GeneratePython.lua
@@ -30,13 +30,45 @@ function GeneratePython:writeTuple(name, content)
     self.file:write("\n\t\t}\n")
 end
 
+function GeneratePython:writeProduction(productions)
+    self.file:write("\t\tself.__production = {")
+    local firstKey = true
+    for index, value in pairs(productions) do
+        self.file:write(string.format("%s\n\t\t\t\"%s\": (", firstKey and "" or ",", index))
+        firstKey = false; local firstList = true
+        for _, production in pairs(value) do
+            if firstList then self.file:write("("); firstList = false
+            else self.file:write(", (")
+            end
+            local first = true
+            for __, derivated in pairs(production) do
+                if first then self.file:write(string.format("\"%s\"", derivated)); first = false
+                else self.file:write(string.format(", \"%s\"", derivated))
+                end
+            end
+            self.file:write(")")
+        end
+        self.file:write(")")
+    end
+    self.file:write("\n\t\t}\n\n")
+end
+
+function GeneratePython:writeGetters(attribute, encapsulation)
+    self.file:write(string.format("\tdef get_%s(self):\n\t\t\treturn %s%s\n\n", attribute, encapsulation, attribute))
+end
+
 function GeneratePython:write(productions)
     self.file:write("class FirstFollow:\n\tdef __init__(self):\n")
     for index, value in pairs(self.info) do
         self.file:write(string.format("\t\tself.%s = \"%s\"\n", index:gsub("%s+", ""), value:gsub("\'", "")))
     end
-    self:writeTuple("first", self.first)
-    self:writeTuple("follow", self.follow)
+    self:writeTuple("__first", self.first)
+    self:writeTuple("__follow", self.follow)
+    if productions then
+        self:writeProduction(productions)
+        self:writeGetters("productions", "__")
+    end
+    self:writeGetters("first", "__"); self:writeGetters("follow", "__")
 end
 
 return GeneratePython
